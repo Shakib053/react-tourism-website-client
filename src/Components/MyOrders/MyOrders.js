a433634ce929cc048e69cd5f1b0a955f9a019608
Shakib053 <75584294+Shakib053@users.noreply.github.com>
2021-10-31 19:18:51 +0600

    id added

--- src/Components/MyOrders/MyOrders.js
+++ src/Components/MyOrders/MyOrders.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAuth from '../../hooks/useAuth';
+import SingleOrder from '../SingleOrder/SingleOrder';
 
 const MyOrders = () => {
+    const { user } = useAuth();
+
+    const [orders, setOrders] = useState([]);
+    useEffect(() => {
+        fetch('https://safe-temple-87819.herokuapp.com/orders')
+            .then(res => res.json())
+            .then(data => setOrders(data));
+    }, []);
+    const ExactData = orders.filter(td => td.email == user.email)
+    console.log(ExactData);
+
     return (
         <div>
-            <p>my ordes</p>
+            <div className="row row-cols-1  g-4">
+                {
+                    ExactData.map(order => <SingleOrder order={order}></SingleOrder>)
+                }
+            </div>
         </div>
     );
 };
